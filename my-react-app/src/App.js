f3b65b49e6d87332100328e530a635e7ce2a9465
Gonzalo Borges <gb_911@msn.com>
2020-07-27 15:07:32 +0200

    Using JSX syntax
    Using JSX syntax to remove the list that requirted to add keys

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -10,17 +10,17 @@ function HelloFunctionComponent(props) {
 const title = React.createElement('h2', {key:"0"}, 'ReactJS Task 1');
 const helloElement = React.createElement('p', {key:"1"}, '- Hello ReactJS using React.createElement');
 const gitHubLink = React.createElement('a', {key:"5", href: "https://github.com/borgesgj/reactjs-mentoring.git"}, 'Find me on GitHub');
-const mainContainer = React.createElement('div', {style: {margin: "20px"},}, [
-  title,
-  helloElement, 
-  <HelloComponent key="2"/>,
-  <HelloPureComponent key="3" iam="React.PureComponent" mynumber="2"/>,
-  <HelloFunctionComponent  key="4" mynumber="4"/>,
-  gitHubLink]);
 
 function App() {
   return (
-    mainContainer
+    <div style={{margin: "20px"}}>
+      {title}
+      {helloElement}
+      <HelloComponent/>
+      <HelloPureComponent iam="React.PureComponent" mynumber="2"/>
+      <HelloFunctionComponent mynumber="4"/>
+      {gitHubLink}
+    </div>
   );
 }
 
