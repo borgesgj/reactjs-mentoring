6d6eb6681063e568f3c90463affd9928495797c9
Gonzalo Borges <gb_911@msn.com>
2020-07-27 10:21:12 +0200

    Fixed console warnings
    Added keys to elelemts, and removed unused variable logo and the picture created from "create-react-app" command

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import HelloComponent from './HelloComponent';
 import HelloPureComponent from './HelloPureComponent';
@@ -8,15 +7,15 @@ function HelloFunctionComponent(props) {
   return <p>- Hello ReactJS using Function Component with mynumber*3={props.mynumber*3}</p>
 }
 
-const title = React.createElement('h2', {}, 'ReactJS Task 1');
-const helloElement = React.createElement('p', {}, '- Hello ReactJS using React.createElement');
-const gitHubLink = React.createElement('a', {href: "https://github.com/borgesgj/reactjs-mentoring.git"}, 'Find me on GitHub');
-const mainContainer = React.createElement('div', {style: {margin: "20px"}}, [
+const title = React.createElement('h2', {key:"0"}, 'ReactJS Task 1');
+const helloElement = React.createElement('p', {key:"1"}, '- Hello ReactJS using React.createElement');
+const gitHubLink = React.createElement('a', {key:"5", href: "https://github.com/borgesgj/reactjs-mentoring.git"}, 'Find me on GitHub');
+const mainContainer = React.createElement('div', {style: {margin: "20px"},}, [
   title,
   helloElement, 
-  <HelloComponent/>,
-  <HelloPureComponent iam="React.PureComponent" mynumber="2"/>,
-  <HelloFunctionComponent mynumber="4"/>,
+  <HelloComponent key="2"/>,
+  <HelloPureComponent key="3" iam="React.PureComponent" mynumber="2"/>,
+  <HelloFunctionComponent  key="4" mynumber="4"/>,
   gitHubLink]);
 
 function App() {
