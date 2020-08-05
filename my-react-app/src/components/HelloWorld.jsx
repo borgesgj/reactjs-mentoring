a3ad0e7720b9a43f1bf4ed864663cd873970aa37
Gonzalo Borges <gb_911@msn.com>
2020-08-05 09:49:11 +0200

    Created hello world code

---
+++ my-react-app/src/components/HelloWorld.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HelloWorld = () => {
+  return (
+    <div>
+      <p>- Hello world on behalf of Gonzalo using React!</p>
+    </div>
+  );
+};
+
+export default HelloWorld;
