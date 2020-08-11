84efc0d43bbaa0c70eeef9b85c0b463b15e71c39
Gonzalo Borges <gb_911@msn.com>
2020-08-11 19:25:43 +0200

    Some styles updated

--- my-react-app/src/components/common/TabControl/TabControl.js
+++ my-react-app/src/components/common/TabControl/TabControl.js
@@ -4,11 +4,9 @@ import './style.less';
 import TabItem from "../TabItem/TabItem";
 
 function TabControl (props) {
-    const keyPrefix = "tab_item_";
-
     var tabItems = props.tabItems;
     var itemList = tabItems.map((item)=>
-        <TabItem key={keyPrefix+item.id} class="left-align" enabled={item.enabled} title={item.title}/>);
+        <TabItem key={item.id} class="left-align" enabled={item.enabled} title={item.title}/>);
 
     return ( 
         <div className="tab-control clearfix">
