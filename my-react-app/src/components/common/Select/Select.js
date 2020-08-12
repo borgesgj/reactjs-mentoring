53b4d72e0cc126e296eb58305d7636f8ae8b757a
Gonzalo Borges <gb_911@msn.com>
2020-08-12 11:59:33 +0200

    Added arrow to the select control

--- my-react-app/src/components/common/Select/Select.js
+++ my-react-app/src/components/common/Select/Select.js
@@ -3,9 +3,15 @@ import "./style.less";
 
 function Select(props) {
     return (
-        <div className="select-box right-align">
-            <select>{props.children}</select>
-        </div>
+        <>
+            
+            <div className="select-box right-align">
+                <div className="select-arrow right-align">
+                    &#8711;
+                </div>
+                <select className="left-align">{props.children}</select>
+            </div>
+        </>
     );
 }
 
